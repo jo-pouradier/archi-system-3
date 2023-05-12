189765b610f9b0863d7e3dc84243172c1d682342
jo-pouradier <joseph.pouradier-duteil@cpe.fr>
2023-05-12 22:40:49 +0200

    nginx marche pas jsp pk

--- proxy/README.md
+++ proxy/README.md
@@ -1,6 +1,32 @@
 # Proxy
 
-Tout est deja fais grace a docker compose, il suffit de lancer la commande suivante dans le dossier proxy:
+## Docker
+
+Installer docker et docker-compose sur votre machine. 
+Sur windows il faut installer docker desktop. Et dans les settings -> advanced -> choisir System. Cela permet a Docker d'installer les bin a la racines. A voir si ca marche aussi sur wsl2.
+
+tester l'installation avec la commande suivante:
+
+```bash
+docker --version
+# et 
+docker run hello-world
+```
+
+Puis pour plus de faciliter ajouter votre user au groupe docker:
+
+```bash
+sudo usermod -aG docker $USER
+```
+Sur mac il faut utiliser la commande suivante:
+
+```bash
+sudo dscl . append /Groups/docker GroupMembership $USER
+```
+
+## Lancer le proxy
+
+Tout est deja fais grace a docker compose, il suffit de lancer la commande suivante dans le dossier proxy (ce dossier):
 
 ```bash
 docker-compose up -d
--- proxy/test.py
+++ proxy/test.py
@@ -1,22 +1,38 @@
 # create 5 flask app listening on 5 different ports: 8080, 8081, 8082, 8083, 8084 that return the port number
 
+from multiprocessing import Process
+import threading
 from flask import Flask
 import os
 
-apps = []
-
-for i in range(1, 6):
-    app = Flask(__name__ + str(i))
-    @app.route('/')
-    def hello_world():
-        return str(os.environ['PORT'])
-    apps.append(app)
-
+def generate_functions():
+    apps = []
+    for i in range(5):
+        app = Flask(__name__ + str(i))
+        @app.route('/')
+        def print_value(i=i):  # capture i as a local variable
+            return f"Value {i}"
+        
+        @app.route('/card')
+        def card():
+            return f"Card {i}"
+        # functions.append(print_value)
+        apps.append(app)
+    return apps
 
 
 if __name__ == '__main__':
+    threads = []
+    apps = generate_functions()
+    i = 0
     for app in apps:
-        app.run(host='127.0.0.1', port=8080+i)
+        thread = threading.Thread(target=app.run, kwargs={'host': 'localhost', 'port': 8080+i})
+        thread.start()
+        threads.append(thread)
+        i += 1
+    
 
+    input("Press Enter to continue...")
+    for thread in threads:
+        thread.join()
 
-# Path: proxy/Dockerfile
