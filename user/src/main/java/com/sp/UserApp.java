3b33f04744a671822267eec9c4bec503e3a9048b
Hugues <hugues.farthouat@cpe.fr>
2023-05-17 14:14:59 +0200

    Rename all projects

--- user/src/main/java/com/sp/UserApp.java
+++ user/src/main/java/com/sp/UserApp.java
@@ -2,12 +2,11 @@ package com.sp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-public class SpAppMain {
+public class UserApp {
 	
 	public static void main(String[] args) {
-		SpringApplication.run(SpAppMain.class,args);
+		SpringApplication.run(UserApp.class,args);
 	}
 }
