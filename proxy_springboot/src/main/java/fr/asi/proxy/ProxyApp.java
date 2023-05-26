a2e5d875609f8eb924142ec81b5bcb58dadb729d
Ixion <hugues.farthouat@cpe.fr>
2023-05-26 09:44:37 +0200

    remove old proxy springboot

--- proxy_springboot/src/main/java/fr/asi/proxy/ProxyApp.java
+++
@@ -1,9 +0,0 @@
-package fr.asi.proxy;
-
-@SpringBootApplication
-public class ProxyApp {
-
-    public static void main(String[] args) {
-        SpringApplication.run(CardApp.class,args);
-    }
-}
