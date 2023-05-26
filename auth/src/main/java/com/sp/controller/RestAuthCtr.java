ea2ce0c791109333c6d54d796cadd7ec2d5d5f70
jo-pouradier <joseph.pouradier-duteil@cpe.fr>
2023-05-26 12:17:07 +0200

    probleme login

--- auth/src/main/java/com/sp/controller/RestAuthCtr.java
+++ auth/src/main/java/com/sp/controller/RestAuthCtr.java
@@ -31,37 +31,37 @@ public class RestAuthCtr {
         System.out.println(data);
         UserDTO user = authService.login(data.getEmail(), data.getPassword());
         if (user == null) return new ResponseEntity<>(null,HttpStatus.FORBIDDEN);
-        UserDTO userDTO = new UserDTO();
-        BeanUtils.copyProperties(user, userDTO);
-        return new ResponseEntity<>(userDTO,HttpStatus.OK);
+        return new ResponseEntity<>(user,HttpStatus.OK);
     }
 
     @PostMapping(value = "/register")
     public UserDTO register(@RequestBody UserRegisterDTO data) {
-        UserDTO user = authService.register(data.getName(),data.getPassword(), data.getEmail()); // on renvoie l'uuid ou null;
-        System.out.println(user);
+        UserDTO userDTO = authService.register(data.getName(), data.getPassword(), data.getEmail()); // on renvoie l'uuid ou null;
+        System.out.println(userDTO);
+        // create user in UserService
+        userDTO = Utils.requestService(EServices.USER_SERVICE, "createUser", userDTO, UserDTO.class, HttpMethod.POST);
+        if (userDTO == null) return null;
         // request card services to create a card set
-        String response = Utils.requestService(EServices.CARD_SERVICE, "newUserSet/"+user.getUUID(), null , String.class, HttpMethod.GET);
+        String response = Utils.requestService(EServices.CARD_SERVICE, "newUserSet/" + userDTO.getUUID(), null, String.class, HttpMethod.GET);
         if (response != null) return null;
-        UserDTO userDTO = new UserDTO();
-        BeanUtils.copyProperties(user, userDTO);
+        userDTO = new UserDTO();
         return userDTO;
     }
 
     @GetMapping(value = "/isUser/{uuid}")
     public ResponseEntity<?> isUser(@PathVariable("uuid") String uuid) {
-        if (uuid == null) return new ResponseEntity<>(null,HttpStatus.BAD_REQUEST);
+        if (uuid == null) return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
         UserDTO userDTO = Utils.getUser(uuid);
-        if (userDTO == null) return new ResponseEntity<>(null,HttpStatus.NOT_FOUND);
-        return new ResponseEntity<>(userDTO,HttpStatus.OK);
+        if (userDTO == null) return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+        return new ResponseEntity<>(userDTO, HttpStatus.OK);
     }
 
     @GetMapping(value = "/getAuthType/{uuid}")
     public ResponseEntity<AuthType> getAuthType(@PathVariable("uuid") String uuid) {
-        if (uuid == null) return new ResponseEntity<>(AuthType.NO_AUTH,HttpStatus.BAD_REQUEST);
+        if (uuid == null) return new ResponseEntity<>(AuthType.NO_AUTH, HttpStatus.BAD_REQUEST);
         AuthType authType = authService.getAuthType(uuid);
-        if (authType == AuthType.NO_AUTH) return new ResponseEntity<>(AuthType.NO_AUTH,HttpStatus.NOT_FOUND);
-        return new ResponseEntity<>(authType,HttpStatus.OK);
+        if (authType == AuthType.NO_AUTH) return new ResponseEntity<>(AuthType.NO_AUTH, HttpStatus.NOT_FOUND);
+        return new ResponseEntity<>(authType, HttpStatus.OK);
     }
 }
 
